e0038917da853dd6fa395ea28bd0b9b1a28a27a5
Joshua Lopez <jrenelopez92@gmail.com>
2021-09-01 13:00:24 -0500

    have images on the webpage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,37 @@ import SearchBar from "./SearchBar";
 import secret from "./SecretFile";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+  // when the call is completed we get a notifcation as a promise this is the old method
+  // onSearchSubmit(term) {
+  //   axios
+  //     .get("https://api.unsplash.com/search/photos", {
+  //       params: { query: term },
+  //       headers: {
+  //         Authorization: secret,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log(response.data.results);
+  //     });
+  // }
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
       headers: {
         Authorization: secret,
       },
     });
-  }
+    this.setState({
+      images: response.data.results,
+    });
+  };
 
   render() {
     return (
       <div className="ui container " style={{ marginTop: "10px" }}>
-        {" "}
-        <SearchBar onSubmit={this.onSearchSubmit} />{" "}
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found : {this.state.images.length}
       </div>
     );
   }
